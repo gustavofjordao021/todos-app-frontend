cd680fdb3d1538073ab19869c514ec9d58899486
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-11-26 11:22:55 -0500

    Login page setup and Hooks refactoring

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import login from "./views/login";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/login" component={login} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+export default App;
---
+++ src/views/login.js
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_FIREBASE_API_ENDPOINT;
+
+const service = axios.create({
+  baseURL,
+});
+
+const styles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10,
+  },
+  progess: {
+    position: "absolute",
+  },
+});
+
+const Login = (props) => {
+  const [emailState, setEmailState] = useState({
+    email: "",
+  });
+  const [passwordState, setPasswordState] = useState({
+    password: "",
+  });
+  const [lifecycleState, setLifecycleState] = useState({
+    errors: [],
+    loading: false,
+  });
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    setLifecycleState({ errors: [], loading: true });
+    const userData = {
+      email: emailState.email,
+      password: passwordState.password,
+    };
+    console.log("Service ===> ", service);
+    console.log("Endpoint ===> ", process.env.FIREBASE_API_ENDPOINT);
+    service
+      .post("/auth/login", userData)
+      .then((response) => {
+        localStorage.setItem("AuthToken", `Bearer ${response.data.token}`);
+        this.setState({
+          errors: [],
+          loading: false,
+        });
+        this.props.history.push("/");
+      })
+      .catch((error) => {
+        setLifecycleState({
+          errors: error.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  const { classes } = props;
+  const { errors, loading } = lifecycleState;
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}></Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            helperText={errors.email}
+            error={errors.email ? true : false}
+            onChange={(e) => setEmailState({ email: e.target.value })}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            helperText={errors.password}
+            error={errors.password ? true : false}
+            onChange={(e) => setPasswordState({ password: e.target.value })}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={(e) => handleSubmit(e)}
+            disabled={
+              lifecycleState.loading ||
+              !emailState.email ||
+              !passwordState.password
+            }
+          >
+            Sign In
+            {loading && (
+              <CircularProgress size={30} className={classes.progress} />
+            )}
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+          {errors.general && (
+            <Typography variant="body2" className={classes.customError}>
+              {errors.general}
+            </Typography>
+          )}
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default withStyles(styles)(Login);
